0bcf60988799e6829301322c5616181ca02a7d01
Arya Girish K <arya.girish.k@ibm.com>
2026-05-25 17:23:15 +0530

    testing

--- examples/icd-version-lister/main.tf
+++ examples/icd-version-lister/main.tf
@@ -1,5 +1,5 @@
 module "icd_version_lister" {
-  source   = "git::https://github.com/arya-girish-k/terraform-ibm-common-utilities.git//modules/icd-versions?ref=anam-patch"
+  source   = "git::https://github.com/arya-girish-k/terraform-ibm-common-utilities.git//modules/icd-versions?ref=17813-script"
   region   = var.region
   icd_type = var.icd_type
 }
